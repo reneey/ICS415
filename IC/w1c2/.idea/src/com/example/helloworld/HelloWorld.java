3761b47edaf55f7cb7ab142d4b4e7779160cc74e
Renee Yonamine <reneey@hawaii.edu>
2015-08-26 10:06:10 -1000

    new version

---
+++ IC/w1c2/.idea/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Renee on 8/25/2015.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
